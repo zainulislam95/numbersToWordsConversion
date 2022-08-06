d3de30ae1bd114d66dba392e157e58f2fbbbbd0b
zainulislam95 <zain.aries95@gmail.com>
2022-08-06 23:25:52 +0500

    Conversion Implemented using TWord

--- numbersToWordsConversion/Program.cs
+++ numbersToWordsConversion/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TWord;
+
+Console.WriteLine("Enter a Number to convert to currency:");
+string number = Console.ReadLine();
+var result = Convert.ToDecimal(number);
+
+IAtWord at = new AtWordBuilder()
+               .SetLanguage(Language.English)
+               .SetCurrency(CurrencySymbol.USD)
+               .Build();
+
+var words = at.ToWords(result);
+Console.WriteLine(words); 
